d6dbea09b44e48bf5100b7d56a6c575b13fc45da
Oleh Didushok <o.didushok@lampalampa.net>
2026-02-06 17:52:50 +0200

    Setup project

---
+++ HP/HP/HPApp.swift
@@ -0,0 +1,17 @@
+//
+//  HPApp.swift
+//  HP
+//
+//  Created by Oleh Didushok on 06.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct HPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
